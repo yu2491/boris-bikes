bb6bfbdef4fe2000dcd7002e157447f9a1c76030
Djura22 <michaeliangroves@gmail.com>
2020-08-05 14:26:13 +0100

    Bike.rb added and require_relative to docking_station.rb

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,4 @@
-require 'bb.rb'
+require 'bike'
 
 describe Bike do
 	it { is_expected.to respond_to('working?') }
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
-require 'bb.rb'
+require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to('release_bike') } 
+  
+  it 'released bike is working?' do
+  	bike = subject.release_bike
+  	expect(bike).to be_working
+  end
 end
+
