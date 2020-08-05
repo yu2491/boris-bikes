615aec1878cc6ba9a0cbcc35add8c54084ad6019
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-05 15:22:54 +0100

    dock method + testadded

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,6 +4,7 @@ class DockingStation
 	def release_bike
     Bike.new
   end
+
+	def dock(bike)
+	end
 end
-
-
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to('release_bike') } 
-  
+  it { is_expected.to respond_to('release_bike') }
+
   it 'released bike is working?' do
   	bike = subject.release_bike
   	expect(bike).to be_working
   end
-end
 
+  it { is_expected.to respond_to('dock') }
+
+end
